0797aff39b0fb0e0e70074897e2376eac4c1c2b7
mamun945 <almamunab2001@gmail.com>
2026-02-23 09:39:46 +0600

    interview function for rander interview card

--- script.js
+++ script.js
@@ -211,3 +211,36 @@
        counter();
         }
     })
+
+
+    // for interview rander
+
+    function  interviewRander(){
+        filterSection.innerHTML ='';
+        interviewArray.forEach( item =>{
+            let div = document.createElement('div');
+            div.className = 'card hover:shadow-md hover:shadow-orange-500 border-1 border-1 rounded-md p-8 flex justify-between';
+            div.innerHTML = `
+              <div class="text space-y-4">
+                    <div>
+                        <h2 class="company-name font-bold text-xl">${item.companyName}</h2>
+                        <p class="position text-[gray]">${item.position}</p>
+                    </div>
+                    <p class="text-[gray]"><span class="location">${item.location}</span> • Full-time • <span class="salary">${item.salary}</span></p>
+                    <div>
+                        <button class="status border-1 border-green-400 text-green-500 px-4 py-2 bg-gray-100 font-bold">${item.status}</button>
+                        <p class="description mt-2">${item.description}</p>
+                    </div>
+                    <div class="interview-reject-button flex gap-6">
+                        <button class="interview-btn border-2 border-green-400 text-green-500 px-4 py-2 font-semibold rounded-md">INTERVIEW</button>
+                        <button class="rejected-btn border-2 border-red-400 text-red-500 px-4 py-2 font-semibold rounded-md"">REJECTED</button>
+                    </div>
+                </div>
+                <div class="delete-btn">
+                  <i id="delete-btn" class="fa-solid fa-trash-can text-2xl"></i>
+                </div>
+            `;
+            filterSection.appendChild(div);
+        });
+     
+    }
